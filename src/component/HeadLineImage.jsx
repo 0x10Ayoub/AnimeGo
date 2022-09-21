736f6bbdd3a8791c25d9bc8e10829f6b3a2731e2
0x10ayoub <ayoubkhourbach@gmail.com>
2022-09-21 23:40:19 +0100

    add image place holder and make headlights fully responsive

--- src/component/HeadLineImage.jsx
+++ src/component/HeadLineImage.jsx
@@ -4,8 +4,8 @@ export default function HeadLineImage({className,imageSource}) {
 
     return (
         <div className={className}>
-            <div className="min-h-full min-w-full bg-local bg-center " 
-                style={{backgroundImage : `url(${imageSource})`,boxShadow : "-12px 0px 95px 53px rgba(0, 9, 36, 0.74) inset"}} >
+            <div className="min-h-full min-w-full bg-local bg-center bg-no-repeat bg-cover" 
+                style={{backgroundImage : `url(${imageSource})`}} >
 
                 </div>
         </div>
--- src/component/HighLights.jsx
+++ src/component/HighLights.jsx
@@ -1,53 +1,55 @@
 import HeadLineImage from "./HeadLineImage"
-import {use, useEffect, useState} from 'react'
+import { use, useEffect, useState } from 'react'
+
 var images = [
     "https://media.kitsu.io/anime/poster_images/8671/large.jpg",
+    "https://media.kitsu.io/anime/44081/poster_image/large-049ba5097e6d6dbb515a0584a7ccd3b2.jpeg",
+    "https://media.kitsu.io/anime/poster_images/12/large.jpg",
+    "https://media.kitsu.io/anime/poster_images/10740/large.jpg",
+    "https://media.kitsu.io/anime/43806/poster_image/large-815d6008fb3b56f4291b9f0ffa05cd8f.jpeg"
 ]
+
 export default function HightLights() {
-    const [height,SetHeight] = useState(600);
+    const [height, SetHeight] = useState(600);
 
-    useEffect(()=>{
-        window.matchMedia("(max-width: 640px)").onchange = getHeigh;
-    },[]);
+    useEffect(() => {
+        var media = window.matchMedia("(max-width: 640px)");
+        media.onchange = getHeigh;
+        getHeigh(media);
+    }, []);
 
-    function getHeigh(e)
-    {
-        console.log(e);
-        var height = e.matches ? 1200 : 600;
+    function getHeigh(e) {
+        var height = e.matches ? 1200 : 720;
         SetHeight(height);
     }
     return (
 
-        <div className="grid grid-cols-10  grid-rows-6 md:grid-rows-6 gap-1 mt-10  " style={{ height: height }}>
-            <HeadLineImage className="bg-blue-500 
+        <div className="grid grid-cols-10  grid-rows-5 sm:grid-rows-6 gap-1 mt-10  " style={{ height: height }}>
+            <HeadLineImage className="
                                     md:col-span-4 md:row-span-6
-                                    sm:col-span-6 sm:row-span-6
-                                    col-start-2 row-start-1 row-span-3 col-span-8 
-                                    " 
-                                    imageSource={images[0]} />
-            <div className="bg-red-500 
+                                    sm:col-span-6 sm:row-span-4
+                                    col-start-1 row-start-1 row-span-3 col-span-10"
+                imageSource={images[0]} />
+            <HeadLineImage className="
                             md:col-start-5 md:row-start-1 md:col-span-4 md:row-span-3 
-                            sm:col-start-1 sm:row-start-7 sm:col-span-5 sm:row-span-3">
-                                red
-            </div>
-            <div className="bg-yellow-500 
+                            sm:col-start-1 sm:row-start-5 sm:col-span-5 sm:row-span-2
+                            row-start-4  col-span-5 row-span-1 "
+                imageSource={images[1]} />
+            <HeadLineImage className="
                             md:col-start-9 md:row-start-1 md:col-span-2 md:row-span-2 
-                            sm:col-start-7 sm:row-start-1 sm:col-span-4 sm:row-span-3 
-            ">
-                yellow
-            </div>
-            <div className="bg-orange-500 
-                md:col-start-5 md:row-start-4 md:col-span-4 md:row-span-3
-                sm:col-start-6 sm:row-start-7 sm:col-span-5 sm:row-span-3
-            ">
-                orange
-            </div>
-            <div className="bg-green-500 
-                    md:col-span-2 md:row-span-4
-                    sm:col-start-7 sm:row-start-4 sm:col-span-4 sm:row-span-3
-            ">
-                green
-            </div>
+                            sm:col-start-7 sm:row-start-1 sm:col-span-4 sm:row-span-2
+                            col-start-6 row-start-4  col-span-5 row-span-1"
+                imageSource={images[3]} />
+            <HeadLineImage className="
+                            md:col-start-5 md:row-start-4 md:col-span-4 md:row-span-3
+                            sm:col-start-6 sm:row-start-5 sm:col-span-5 sm:row-span-2
+                            row-start-5  col-span-5 row-span-1"
+                imageSource={images[2]} />
+            <HeadLineImage className="
+                            md:col-span-2 md:row-span-4
+                            sm:col-start-7 sm:row-start-3 sm:col-span-4 sm:row-span-2
+                            col-start-6 row-start-5  col-span-5 row-span-1"
+                imageSource={images[4]} />
         </div>
     )
 }
