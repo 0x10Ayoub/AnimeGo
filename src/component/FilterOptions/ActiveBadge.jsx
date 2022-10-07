b9cab75ac043a7a04e65e9421464881578a230b0
0x10ayoub <ayoubkhourbach@gmail.com>
2022-10-07 19:29:55 +0100

    add font awesome

--- src/component/FilterOptions/ActiveBadge.jsx
+++ src/component/FilterOptions/ActiveBadge.jsx
@@ -1,5 +1,7 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleCheck } from "@fortawesome/free-solid-svg-icons";
 export default function ActiveBadge() {
     return (
-        <span className="block m-auto">Active</span>
+        <span className="block m-auto mr-5 text-right"><FontAwesomeIcon className="text-cyan-400" icon={faCircleCheck} /></span>
     );
 }
--- src/component/FilterOptions/SingleYear.jsx
+++ src/component/FilterOptions/SingleYear.jsx
@@ -2,7 +2,7 @@ export default  function SingleYear({value,ActiveBadge})
 {
     return (
         <div   value={value} className=" flex justify-start hover:cursor-pointer hover:text-cyan-400 hover:bg-slate-100">
-            <span className="block text-left ml-2">{value}</span>
+            <span className="block text-left ml-5">{value}</span>
             { ActiveBadge && <ActiveBadge />}
         </div>
     )
